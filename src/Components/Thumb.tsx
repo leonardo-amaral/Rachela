da7c0d0173a98161499462082c3bd3cc380041ed
Leonardo Amaral <leozero103@gmail.com>
2022-05-11 12:14:48 -0300

    .

--- src/Components/Thumb.tsx
+++ src/Components/Thumb.tsx
@@ -15,8 +15,9 @@ function Thumb() {
   return (
     <div className='thumb'>
       <div className="content-thumb">
-        <h1 className='title-thumb'>Lorem ipsum dolor sit amet</h1>
+        <h1 className='title-thumb'>Make your food with Rachela.</h1>
         <p className='desc-thumb'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate beatae accusamus quis quia, aspernatur enim facilis pariatur nostrum neque. Vero, dolore quia soluta cum facere quas in unde ipsa aliquam?</p>
+        <button className='btn-thumb'>Make my order!</button>
       </div>
     </div>
   )
